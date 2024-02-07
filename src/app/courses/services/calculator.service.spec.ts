e083172c39da0ea4ee126361fc4b0a9a3bd6df30
Michał Raźny <michal.razny23@gmail.com>
2024-02-07 21:19:46 +0100

    feat(services): add unit tests for courses and calculator services

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from '@angular/core/testing';
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+
+// xdescribe will skip while test
+// fdescribe only those will be executed
+describe('CalculatorService', () => {
+
+    let calculator: CalculatorService;
+    let loggerSpy: any;
+
+    beforeEach(() => {
+        // we provide fake service, only testing service should have its instance
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+        // if function of spy service returns value we can mock it:
+        // logger.log.and.returnValue();
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                { provide: LoggerService, useValue: loggerSpy }
+            ]
+        });
+
+        calculator = TestBed.inject(CalculatorService);
+    });
+
+    // xit will skip single test case
+    // fit will only execute selected test cases
+    it('should add two numbers', () => {
+        const result = calculator.add(2, 2);
+        expect(result).toBe(4);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers', () => {
+        const result = calculator.subtract(2, 2);
+        expect(result).toBe(0);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+});
